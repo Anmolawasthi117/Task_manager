d40a654677ca1f819fe26ad04a0cddcd0956c86e
Anmol <anmolawasthi117@gmail.com>
2025-08-03 01:38:00 +0530

    fix: update baseURL in axios instance to include API version

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://task-manager-nvld.vercel.app/",  
+  baseURL: "https://task-manager-nvld.vercel.app/api/v1/",  
   withCredentials: true,                   
 });
 
