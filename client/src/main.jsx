acf775f78c6cc58bdefce77004b0287a95418f76
Anmol <anmolawasthi117@gmail.com>
2025-08-03 13:17:16 +0530

    fix: refactor main.jsx to include BrowserRouter for routing support

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,22 @@
-// main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import { store } from './store/store';
+import './index.css';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
       <ToastContainer position="top-right" autoClose={2500} />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
