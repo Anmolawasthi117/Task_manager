fb65127e7a68edeb48b2819e91881a360ce33e25
Anmol <anmolawasthi117@gmail.com>
2025-08-03 13:11:47 +0530

    fix: correct import path for store in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "./store/store.js";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
